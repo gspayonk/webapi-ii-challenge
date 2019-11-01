f69e97116a59d767583120b2b791a1cdf2f97dfe
gspayonk <grissobel.castro@gmail.com>
2019-10-31 20:35:53 -0400

    nodemon

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ server.use(express.json());
 
 server.use("/api/posts", postsRouter);
 
+server.get('/', (req,res) =>{
+    res.status(200).send({hello:'Web 23'});
+});
+
 server.listen(8000, () => console.log("server is listening on 8000"));
